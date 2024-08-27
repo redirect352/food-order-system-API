58dcd8437d5e442d4493a4ae5e3caac253debdcf
Ponomarev Nikita <79397499+redirect352@users.noreply.github.com>
2024-08-27 15:30:18 +0300

    added check on user active status

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +18,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { userId: payload.id, role: payload.role };
+    const user = await this.userService.findById(payload.id);
+    if (!user || !user.employeeBasicData.active || user.role !== payload.role)
+      return null;
+    return { userId: user.id, role: user.role };
   }
 }
